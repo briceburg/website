22546c22f796bb3cb63a4c50718dc8690a918382
RMacy <ryan.w.macy@gmail.com>
2013-03-08 23:29:40 -0600

    switched to mongo, using redis for sessions, hasing out schemas & endpoints

--- app.js
+++ app.js
@@ -1,73 +1,97 @@
+// Requires
+
 var express    = require('express')
   , url        = require('url')
-  , redis      = require('redis')
+  , util       = require('util')
+  , mongoose   = require('mongoose').connect(process.env.MONGOLAB_URL)
   , RedisStore = require('connect-redis')(express)
   , passport   = require('passport')
   , Github     = require('passport-github').Strategy
   , BitBucket  = require('passport-bitbucket').Strategy
 
+// Setup Express
+
 var redis_url = url.parse(process.env.OPENREDIS_URL)
-
 var app = express()
-  , db  = redis.createClient(redis_url.port, redis_url.hostname).auth(redis_url.auth.split(':')[1])
-
-var storeConfig = {
+var db  = mongoose.connection
+var SessionStore = new RedisStore({
     host: redis_url.hostname
   , port: redis_url.port
   , pass: redis_url.auth.split(':')[1]
-}
+})
 
 app.use(express.logger())
 app.use(express.bodyParser())
 app.use(express.methodOverride())
 app.use(express.cookieParser())
 app.use(express.session({
-  store: new RedisStore(storeConfig)
+  store: SessionStore
 , secret: '802894782dd90d95ed3b7e6d997bd9bae09d13446a133a28a965840645f40b85'
 }))
 app.use(passport.initialize())
-app.use(passport.session())
+app.use(passport.session({
+  store: SessionStore
+, secret: 'cf6078f4a5481fe7011e61271794de01e72f0aabc6b5686fe0101de94a892964'
+}))
 app.use(app.router)
-
 app.set('views', __dirname + '/views')
 app.use(express.static(__dirname + '/public'))
 
-passport.serializeUser(function(user, done){
+// Setup Mongo
 
+db.on('error', console.error.bind(console, 'connection error:'))
+
+var userSchema = mongoose.Schema({
+  username: String
+, service:  String
+, gravatar: String
+, created: {type: Date, default: Date.now}
 })
 
-passport.deserializeUser(function(user, done){
+var User = mongoose.model('User', userSchema)
 
+var jobSchema = mongoose.Schema({
+  slug:     String
+, body:     String
+, username: String
+, created:  {type: Date, deault: Date.now}
+, edited:   {type: Date}
 })
 
-// TODO: reset bitbucker & github keys, put into env, and remove from version contol.
+var Job = mongoose.model('Job', jobSchema)
+
+// Setup Passport
+
+// TODO: reset bitbucket & github keys, put into env, and remove from version contol.
 
 passport.use(new BitBucket({
     consumerKey: "LNkTZHv6bKRCaY94Ac"
   , consumerSecret: "rQX5ptmNKGmG6QYGghAfHtarETctc7RD"
-  , callbackURL: "http://nashjs.org/auth/bitbucket/callback"
+  , callbackURL: "http://staging.nashjs.jit.su/auth/bitbucket/callback"
   },
   function(token, tokenSecret, profile, done) {
-    User.findOrCreate({bitbucketId: profile.username}, function(err, user){
-      return done(err, user)
-    })
+
   }
 ))
 
 passport.use(new Github({
     clientID: "f5c39d4f957679bc7c2e"
   , clientSecret: "4b9fad5294aed0080a77881e71d2c925666ec30d"
-  , callbackURL: "http://nashjs.org/auth/github/callback"
+  , callbackURL: "http://staging.nashjs.jit.su/auth/github/callback"
   },
   function(accessToken, refreshToken, profile, done) {
-    User.findOrCreate({githubId: profile.username}, function(err, user){
-      return done(err, user)
-    })
+
   }
 ))
 
+// Routes
+
 app.get('/', function(req, res){
   res.render('index.jade')
+})
+
+app.get('/login', function(req, res){
+  res.render('login.jade')
 })
 
 app.get('/logout', function(req, res){
@@ -76,31 +100,26 @@ app.get('/logout', function(req, res){
 })
 
 app.get('/auth/bitbucket', passport.authenticate('bitbucket'))
-
-app.get('/auth/bitbucket/callback', passport.authenticate('bitbucket', {successRedirect: '/', failureRedirect: '/'}), function(req, res) {
-  res.send(404)
-})
-
+app.get('/auth/bitbucket/callback', passport.authenticate('bitbucket', {successRedirect: '/', failureRedirect: '/'}))
 app.get('/auth/github', passport.authenticate('github'))
-
-app.get('/auth/github/callback', passport.authenticate('github', {successRedirect: '/', failureRedirect: '/'}), function(req, res) {
-  res.send(404)
-})
+app.get('/auth/github/callback', passport.authenticate('github', {successRedirect: '/', failureRedirect: '/'}))
 
 app.get('/jobs', function(req, res) {
   res.render('jobs.jade')
 })
 
-app.get('/jobs/:id', function(req, res) {
+app.get('/job/:id', function(req, res) {
   res.render('job.jade')
 })
 
-app.get('/jobs/:id/edit', function(req, res) {
+app.get('/job/:id/edit', function(req, res) {
   res.render('job-edit.jade')
 })
 
-app.get('/jobs/:id/destroy', function(req, res) {
-  res.send(404)
+app.get('/job/:id/destroy', function(req, res) {
+  res.redirect('/jobs')
 })
+
+// Serve
 
 app.listen(8080)
