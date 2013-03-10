9d0642a1253ddd624c094e4850668c893f43b537
RMacy <ryan.w.macy@gmail.com>
2013-03-09 21:25:49 -0600

    remove the crufty stuff, specify jade version

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
 var express = require('express')
 var app = express()
 
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jade')
-app.use(express.logger())
-app.use(express.bodyParser())
-app.use(express.methodOverride())
-app.use(express.cookieParser())
-app.use(app.router)
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(req, res){
-  res.render('index')
+  res.render('index.jade')
 })
 
 app.listen(8080)
